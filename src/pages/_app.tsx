24de5f4eabd1b88311c3d9b5a6258a788f4ee46b
marcellasasso <marcellasasso@gmail.com>
2023-05-20 15:42:37 -0300

    added next/image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import logoImg from '@/assets/logo.svg'
 
 import { Container, Header } from '@/styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
