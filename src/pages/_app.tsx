c18e79da4ea647640aa93ba7e7513cf2eba64da3
marcellasasso <marcellasasso@gmail.com>
2023-05-20 15:32:35 -0300

    cabeçalho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+
+import { Container, Header } from '@/styles/pages/app'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header></header>
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
 
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
